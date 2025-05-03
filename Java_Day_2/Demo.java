10e7bcb468eb28f5cdc6418cfee171b58512e758
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-05-03 16:42:39 +0530

    Day_2

---
+++ Java_Day_2/Demo.java
@@ -0,0 +1,11 @@
+import animals.Dog;
+
+
+public class Demo extends Dog
+{
+    public static void main(String args[])
+    {
+        Demo d = new Demo();
+        System.out.println(d.name);
+    }
+}
