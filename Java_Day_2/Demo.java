2e58727798670621e73c03f4257ea4f14c7d496f
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-05-04 09:08:56 +0530

    Day_2

--- Java_Day_2/Demo.java
+++ Java_Day_2/Demo.java
@@ -1,5 +1,9 @@
 import animals.Dog;
 
+class Protect 
+{
+    protected String type = "Protecting";
+}
 
 public class Demo extends Dog
 {
@@ -7,5 +11,8 @@ public class Demo extends Dog
     {
         Demo d = new Demo();
         System.out.println(d.name);
+
+        Protect p = new Protect();
+        System.out.println(p.type);
     }
 }
